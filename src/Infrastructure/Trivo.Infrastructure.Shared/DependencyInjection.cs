3da0f88bc8b7c73e68647a7a194da25d5d3a389b
WilmerDev <wilmerdelacruz.dev@gmail.com>
2026-02-08 20:11:46 -0400

    feat(infrastructure): add shared infrastructure layer with Cloudinary and Email services

---
+++ src/Infrastructure/Trivo.Infrastructure.Shared/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Trivo.Application.Interfaces.Services;
+using Trivo.Domain.Configurations;
+using Trivo.Infrastructure.Shared.Services;
+
+namespace Trivo.Infrastructure.Shared;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructureShared(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddServices();
+        services.AddConfiguration(configuration);
+    }
+
+    private static void AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IEmailService, EmailService>();
+        services.AddScoped<ICloudinaryService, CloudinaryService>();
+    }
+
+    private static void AddConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<EmailSetting>(configuration.GetSection("EmailSetting"));
+        services.Configure<CloudinarySetting>(configuration.GetSection("CloudinarySetting"));
+    }
+}
