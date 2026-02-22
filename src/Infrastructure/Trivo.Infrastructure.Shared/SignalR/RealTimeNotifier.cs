720396fff465a18f70cb3bacad239cff0673e093
Dayron <dayronbp06@gmail.com>
2026-02-23 06:26:27 +1400

    feat: add chat module

---
+++ src/Infrastructure/Trivo.Infrastructure.Shared/SignalR/RealTimeNotifier.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Trivo.Application.DTOs;
+using Trivo.Application.DTOs.Chat;
+using Trivo.Application.Interfaces.SignalR;
+using Trivo.Infrastructure.Shared.SignalR.Hubs;
+
+namespace Trivo.Infrastructure.Shared.SignalR;
+
+public class RealTimeNotifier(IHubContext<ChatHub, IChatHub> hub) : IRealTimeNotifier
+{
+    public Task NotifyPrivateMessageAsync(MessageDto message, Guid userId)
+        => hub.Clients.User(userId.ToString())
+            .ReceivePrivateMessage(message);
+
+    public Task NotifyMatchConfirmedAsync(Guid userId, string content)
+        => hub.Clients.User(userId.ToString())
+            .NotifyNewMatch(content);
+
+    public Task NotifyMatchPendingAsync(Guid userId, string content)
+        => hub.Clients.User(userId.ToString())
+            .NotifyPendingMatch(content);
+
+    public async Task NotifyNewChatAsync(Guid userId, IEnumerable<ChatDto> chats)
+    {
+        foreach (var chat in chats)
+        {
+            await hub.Clients.User(userId.ToString())
+                .ReceiveNewChat(chat);
+        }
+    }
+
+    public async Task NotifyPagedChatsAsync(Guid userId, IEnumerable<ChatDto> chats)
+    {
+        await hub.Clients.User(userId.ToString())
+            .ReceiveChats(chats);
+    }
+
+    public async Task NotifyMessagePageAsync(Guid userId, Guid chatId, IEnumerable<MessageDto> messagePage)
+    {
+        await hub.Clients.User(userId.ToString())
+            .ReceiveChatMessages(chatId, messagePage);
+    }
+}
