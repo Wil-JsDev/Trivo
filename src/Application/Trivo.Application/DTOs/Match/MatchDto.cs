fc1fd7b93bd1d70fa09df9e5e3ef6dfd075c11b8
Dayron <dayronbp06@gmail.com>
2026-02-23 06:24:26 +1400

    feat: add match module

---
+++ src/Application/Trivo.Application/DTOs/Match/MatchDto.cs
@@ -0,0 +1,16 @@
+﻿using Trivo.Application.DTOs.Experts;
+using Trivo.Application.DTOs.Recruiter;
+
+namespace Trivo.Application.DTOs.Match;
+
+public record MatchDto(
+    Guid MatchId,
+    Guid? RecruiterId,
+    Guid? ExpertId,
+    string? ExpertStatus,
+    string? RecruiterStatus,
+    string? MatchStatus,
+    DateTime? CreatedAt,
+    ExpertAiRecommendationDto? ExpertDto,
+    RecruiterAiRecommendationDto? RecruiterDto
+    );
---
+++ src/Application/Trivo.Application/Interfaces/SignalR/IMatchHub.cs
@@ -0,0 +1,14 @@
+﻿using Trivo.Application.DTOs.Match;
+
+namespace Trivo.Application.Interfaces.SignalR;
+
+public interface IMatchHub
+{
+    Task ReceiveMatchesAsync(IEnumerable<MatchDto> matches);
+
+    Task ReceiveNewMatchesAsync(IEnumerable<MatchDto> matches);
+
+    Task ReceiveMatchCompletedAsync(Guid matchId, MatchDetailsDto matchDetails);
+
+    Task ReceiveMatchRejectedAsync(Guid matchId, MatchDetailsDto matchDetails);
+}
