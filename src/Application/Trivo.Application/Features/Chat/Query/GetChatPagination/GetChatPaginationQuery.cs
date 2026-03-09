91614b399e843e3501f0344b3ae8b3ff5cd45a86
Dayron <dayronbp06@gmail.com>
2026-03-08 20:58:09 -0400

    feat: add chat

---
+++ src/Application/Trivo.Application/Features/Chat/Query/GetChatPagination/GetChatPaginationQuery.cs
@@ -0,0 +1,11 @@
+﻿using Trivo.Application.Abstractions.Messages;
+using Trivo.Application.DTOs.Chat;
+using Trivo.Application.Pagination;
+
+namespace Trivo.Application.Features.Chat.Query.GetChatPagination;
+
+public sealed record GetChatPaginationQuery(
+    Guid UserId,
+    int PageNumber,
+    int PageSize
+) : IQuery<PagedResult<ChatDto>>;
